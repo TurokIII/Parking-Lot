6ff850309b5880fce87ea1c45c52fda547f8bf21
Arturo Hernandez <arturo.hernandez5@gmail.com>
2021-02-16 17:40:42 -0600

    added functionality for handling 1 action in a 2 car lot

--- Main.kt
+++ Main.kt
@@ -1,7 +1,24 @@
 package parking
 
 fun main() {
-    println("White car has parked.")
-    println("Yellow car left the parking lot.")
-    println("Green car just parked here.")
+    val input = readLine()!!
+    val command = input.split(" ")[0]
+    val lot = arrayOf(true, false)
+
+    if (command == "park") {
+        val color = input.split(" ")[2]
+        println("$color car parked in spot 2.")
+        lot[1] = true
+    }
+
+    if (command == "leave") {
+        val spot = input.split(" ")[1].toInt()
+
+        if (lot[spot - 1]) {
+            println("Spot $spot is free.")
+        } else {
+            println("There is no car in spot $spot.")
+        }
+    }
+
 }
